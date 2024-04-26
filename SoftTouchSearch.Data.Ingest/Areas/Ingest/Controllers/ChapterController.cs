82bdf3a73ad0129adb965e6af1ab8e12b4a87381
Jack Kelly <mail@jackkelly.uk>
2024-04-26 15:11:42 +0100

    Chapter ingest controller

--- SoftTouchSearch.Data.Ingest/Areas/Ingest/Controllers/ChapterController.cs
+++ SoftTouchSearch.Data.Ingest/Areas/Ingest/Controllers/ChapterController.cs
@@ -6,17 +6,53 @@ namespace SoftTouchSearch.Ingest.Controllers
 {
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using SoftTouchSearch.Data;
+    using SoftTouchSearch.Data.Ingest.Models;
     using SoftTouchSearch.Data.Models;
 
     /// <summary>
     /// Ingest controller for <see cref="Chapter"/> records.
     /// </summary>
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="ChapterController"/> class.
+    /// </remarks>
+    /// <param name="context">Database Context.</param>
     [Area("Ingest")]
-    public class ChapterController : Controller
+    public class ChapterController(StoryDbContext context) : Controller
     {
-        public IActionResult Index()
+        /// <summary>
+        /// Database context dependancy.
+        /// </summary>
+        private readonly StoryDbContext context = context;
+
+        /// <summary>
+        /// Insert a new <see cref="Chapter"/> into the database.
+        /// </summary>
+        /// <param name="data">DTO containing the chapter data.</param>
+        /// <returns>A <see cref="StatusCodeResult"/> indicating whether the chapter was added.</returns>
+        [HttpPost]
+        public async Task<IActionResult> IndexAsync(ChapterDTO data)
         {
-            return StatusCode(StatusCodes.Status302Found);
+            Chapter? existingChapter = await this.context.Chapters
+                .Where(chapter => chapter.Number == data.Number)
+                .SingleOrDefaultAsync();
+            if (existingChapter != null)
+            {
+                // Chapter already exists, no action necessary.
+                return this.StatusCode(StatusCodes.Status202Accepted);
+            }
+
+            Chapter newChapter = new()
+            {
+                Id = Guid.NewGuid(),
+                Number = data.Number,
+                Title = data.Title,
+            };
+
+            this.context.Chapters.Add(newChapter);
+            await this.context.SaveChangesAsync();
+            return this.StatusCode(StatusCodes.Status201Created);
         }
     }
 }
---
+++ SoftTouchSearch.Data.Ingest/Areas/Ingest/Models/ChapterDTO.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="ChapterDTO.cs" company="Jack Kelly">
+// Copyright (c) Jack Kelly. All rights reserved.
+// </copyright>
+
+namespace SoftTouchSearch.Data.Ingest.Models
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// DTO for Chapter Ingest.
+    /// </summary>
+    [JsonObject]
+    public class ChapterDTO
+    {
+        /// <summary>
+        /// Gets or sets chapter number.
+        /// </summary>
+        [JsonProperty(Required = Required.Always)]
+        public required int Number { get; set; }
+
+        /// <summary>
+        /// Gets or sets chapter title.
+        /// </summary>
+        [JsonProperty(Required = Required.Always)]
+        public required string Title { get; set; }
+    }
+}
