82ed9b495cd26c998a8336e91714bcf12ea52fbd
Jack Kelly <mail@jackkelly.uk>
2024-04-26 15:52:41 +0100

    ExclusionRule ingest controller

---
+++ SoftTouchSearch.Data.Ingest/Areas/Ingest/Controllers/ExclusionController.cs
@@ -0,0 +1,59 @@
+﻿// <copyright file="ExclusionController.cs" company="Jack Kelly">
+// Copyright (c) Jack Kelly. All rights reserved.
+// </copyright>
+
+namespace SoftTouchSearch.Ingest.Controllers
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using SoftTouchSearch.Data;
+    using SoftTouchSearch.Data.Ingest.Models;
+    using SoftTouchSearch.Data.Models;
+
+    /// <summary>
+    /// Ingest controller for <see cref="ExclusionRule"/> records.
+    /// </summary>
+    /// <param name="context">Database Context.</param>
+    [Area("Ingest")]
+    public class ExclusionController(StoryDbContext context) : Controller
+    {
+        /// <summary>
+        /// Database context dependancy.
+        /// </summary>
+        private readonly StoryDbContext context = context;
+
+        /// <summary>
+        /// Insert a new <see cref="ExclusionRule"/> into the database.
+        /// </summary>
+        /// <param name="data">DTO containing the exclusion data.</param>
+        /// <returns>A <see cref="StatusCodeResult"/> indicating whether the chapter was added.</returns>
+        [HttpPost]
+        public async Task<IActionResult> IndexAsync(ExclusionDTO data)
+        {
+            // Convert the different enum definitions
+            int typeInt = (int)data.Type;
+            ExclusionRule.ExclusionType type = (ExclusionRule.ExclusionType)typeInt;
+
+            ExclusionRule? existingRule = await this.context.ExclusionRules
+                .Where(rule => rule.Type == type)
+                .SingleOrDefaultAsync();
+            if (existingRule != null)
+            {
+                // Rule already exists, no action necessary.
+                return this.StatusCode(StatusCodes.Status202Accepted);
+            }
+
+            ExclusionRule newRule = new()
+            {
+                Id = Guid.NewGuid(),
+                Type = type,
+                Value = data.Value,
+            };
+
+            this.context.ExclusionRules.Add(newRule);
+            await this.context.SaveChangesAsync();
+            return this.StatusCode(StatusCodes.Status201Created);
+        }
+    }
+}
---
+++ SoftTouchSearch.Data.Ingest/Areas/Ingest/Models/ExclusionDTO.cs
@@ -0,0 +1,46 @@
+﻿// <copyright file="ExclusionDTO.cs" company="Jack Kelly">
+// Copyright (c) Jack Kelly. All rights reserved.
+// </copyright>
+
+namespace SoftTouchSearch.Data.Ingest.Models
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// DTO for Exclusion rule ingest.
+    /// </summary>
+    [JsonObject]
+    public class ExclusionDTO
+    {
+        /// <summary>
+        /// Supported exclusion types.
+        /// </summary>
+        public enum ExclusionType
+        {
+            /// <summary>
+            /// Filter by title text contents.
+            /// </summary>
+            TitleContains = 1,
+
+            /// <summary>
+            /// Filter by episode text contents.
+            /// </summary>
+            ContentContains = 2,
+
+            /// <summary>
+            /// Filter by description text contents.
+            /// </summary>
+            DescriptionContains = 3,
+        }
+
+        /// <summary>
+        /// Gets or sets type of rule to apply.
+        /// </summary>
+        public required ExclusionType Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets value to match against.
+        /// </summary>
+        public required string Value { get; set; }
+    }
+}
