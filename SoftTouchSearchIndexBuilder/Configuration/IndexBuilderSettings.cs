78f52de238a7eff7c6787108c0d0fddc7208d3c7
Jack Kelly <mail@jackkelly.uk>
2025-04-05 23:53:19 +0100

    Updated SoftTouchSearchIndexBuilder to use new database contexts.

---
+++ SoftTouchSearchIndexBuilder/Configuration/IndexBuilderSettings.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="IndexBuilderSettings.cs" company="Jack Kelly">
+// Copyright (c) Jack Kelly. All rights reserved.
+// </copyright>
+
+namespace SoftTouchSearchIndexBuilder.Configuration
+{
+    /// <summary>
+    /// Options pattern model for configuration of SoftTouchSearchIndexBuilder.
+    /// </summary>
+    /// <seealso href="https://learn.microsoft.com/en-us/dotnet/core/extensions/options"/>
+    public sealed class IndexBuilderSettings
+    {
+        /// <summary>
+        /// Gets or sets the path to the SQLite file for the import database.
+        /// </summary>
+        public string ImportDbPath { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the path to use as the export folder.
+        /// </summary>
+        public string ExportFolderPath { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the file name (including extension) to use for the production database.
+        /// </summary>
+        public string ExportDbFileName { get; set; } = "softtouchsearch.db";
+
+        /// <summary>
+        /// Gets or sets the folder name to use for the search index files.
+        /// </summary>
+        public string ExportIndexFolderName { get; set; } = "softtouchsearch_index";
+    }
+}
--- SoftTouchSearchIndexBuilder/Program.cs
+++ SoftTouchSearchIndexBuilder/Program.cs
@@ -6,9 +6,17 @@ namespace SoftTouchSearchIndexBuilder
 {
     using System.Diagnostics;
     using System.Runtime.InteropServices;
+    using Lucene.Net.Analysis;
+    using Lucene.Net.Analysis.Standard;
     using Lucene.Net.Documents;
+    using Lucene.Net.Index;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
     using SoftTouchSearch.Data;
+    using SoftTouchSearch.Data.Models;
+    using SoftTouchSearch.Index;
+    using SoftTouchSearchIndexBuilder.Classes;
+    using SoftTouchSearchIndexBuilder.Configuration;
 
     /// <summary>
     /// Builds a Lucene.NET index for use by SoftTouchSearch.
@@ -27,70 +35,223 @@ namespace SoftTouchSearchIndexBuilder
         /// <summary>
         /// Builds a Lucene.NET index for use by SoftTouchSearch.
         /// </summary>
-        private static void Main(string[] args)
+        /// <param name="args">Command-line arguments.</param>
+        internal static void Main(string[] args)
         {
-            Stopwatch stopwatchTotal = Stopwatch.StartNew();
+            // Load configuration (arguments or secrets file)
+            Console.Write("Starting...".PadRight(PadRight));
 
-            // Verify arguments
-            if (args.Length != 2)
+            IndexBuilderSettings settings;
+            try
             {
-                throw new ArgumentException("Expected exactly 2 command-line arguments. (DB and export path)");
+                settings = LoadConfiguration(args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.Error.WriteLine(ex.Message);
+                throw;
             }
 
-            string dbPath = args[0];
-            string exportPath = args[1];
-
-            // Create the export folders if they do not exist
-            if (!Directory.Exists(exportPath))
+            // 1a. Create the export folders if they do not already exist
+            if (!Directory.Exists(settings.ExportFolderPath))
             {
-                Directory.CreateDirectory(exportPath);
+                Directory.CreateDirectory(settings.ExportFolderPath);
             }
 
-            string indexDestinationPath = Path.Combine(exportPath, "softtouchsearch_index");
+            string indexDestinationPath = Path.Combine(settings.ExportFolderPath, settings.ExportIndexFolderName);
             if (!Directory.Exists(indexDestinationPath))
             {
                 Directory.CreateDirectory(indexDestinationPath);
             }
 
-            // 1. Copy the database to the export directory
-            Console.Write("Copying Database...".PadRight(PadRight));
+            // 1b. Open the import and export database contexts.
+            string dbDestinationPath = Path.Combine(settings.ExportFolderPath, settings.ExportDbFileName);
 
-            string dbDestinationPath = Path.Combine(exportPath, "softtouchsearch.db");
-            File.Copy(dbPath, dbDestinationPath, true);
+            ImportDbContext importContext = new($"Data Source={settings.ImportDbPath}");
+            _ = importContext.Database.EnsureCreated();
 
-            // 2. Get episodes from database
+            SearchDbContext exportContext = new($"Data Source={dbDestinationPath}");
+            _ = exportContext.Database.EnsureDeleted();
+            exportContext.Database.Migrate();
+
+            // 2a. Transfer Chapters
             Console.WriteLine($"DONE");
-            Console.Write("Loading Episodes...".PadRight(PadRight));
+            Console.Write("Transferring Chapters...".PadRight(PadRight));
 
-            DbContextOptions<StoryDbContext> options = new DbContextOptionsBuilder<StoryDbContext>()
-                .UseSqlite($"Data Source={dbDestinationPath}")
-                .Options;
-            StoryDbContext context = new(options);
-            IndexBuilderMethods methods = new(context, indexDestinationPath);
+            List<Chapter> chaptersExport;
+            try
+            {
+                List<ChapterImport> chaptersImport = importContext.Chapters
+                    .OrderBy(chapter => chapter.Number)
+                    .ToList();
 
-            IEnumerable<Document> documents = methods.ConvertEpisodes();
+                chaptersExport = chaptersImport
+                    .Select(chapter => new Chapter()
+                    {
+                        Id = chapter.Id,
+                        Number = chapter.Number,
+                        Title = chapter.Title,
+                        Episodes = [],
+                    })
+                    .ToList();
 
-            // 3. Build index
+                exportContext.Chapters
+                    .AddRange(chaptersExport);
+
+                exportContext
+                    .SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.Error.WriteLine(ex.Message);
+                throw;
+            }
+
+            // 2b. Transfer Episodes
+            Console.WriteLine($"DONE");
+            Console.Write("Transferring Episodes...".PadRight(PadRight));
+
+            List<Guid> episodeIds;
+            try
+            {
+                List<ExclusionRule> exclusionRules = importContext.ExclusionRules
+                    .ToList();
+
+                // Don't fetch all the episodes at once (small memory optimisation)
+                episodeIds = importContext.Episodes
+                    .OrderBy(episode => episode.EpisodeNumber)
+                    .Select(episode => episode.Id)
+                    .ToList();
+
+                foreach (Guid id in episodeIds)
+                {
+                    EpisodeImport episodeImport = importContext.Episodes
+                        .Where(episode => episode.Id == id)
+                        .Single();
+
+                    Chapter matchingChapter = chaptersExport
+                        .Where(chapter => chapter.Id == episodeImport.ChapterId)
+                        .Single();
+
+                    bool isNonStory = exclusionRules
+                        .Any(rule => rule.CheckEpisodeExcluded(episodeImport));
+
+                    Episode episodeExport = new()
+                    {
+                        Id = episodeImport.Id,
+                        ChapterId = episodeImport.ChapterId,
+                        EpisodeNumber = episodeImport.EpisodeNumber,
+                        Title = episodeImport.Title,
+                        PublishDate = episodeImport.PublishDate,
+                        UrlExternal = episodeImport.UrlExternal,
+                        IsNonStory = isNonStory,
+                        UrlTapas = episodeImport.UrlTapas,
+                        Chapter = matchingChapter,
+                    };
+
+                    exportContext.Episodes
+                        .Add(episodeExport);
+
+                    exportContext
+                        .SaveChanges();
+                }
+
+                GC.Collect(GC.MaxGeneration, GCCollectionMode.Aggressive, true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.Error.WriteLine(ex.Message);
+                throw;
+            }
+
+            // 3. Build search index
             Console.WriteLine($"DONE");
             Console.Write("Building Index...".PadRight(PadRight));
 
-            methods.BuildIndex(documents);
+            try
+            {
+                string indexPath = Path.Combine(settings.ExportFolderPath, settings.ExportIndexFolderName);
+                Lucene.Net.Store.Directory indexDirectory = Lucene.Net.Store.FSDirectory.Open(indexPath);
+
+                Analyzer analyzer = new StandardAnalyzer(Constants.AppLuceneVersion);
+                IndexWriterConfig indexConfig = new(Constants.AppLuceneVersion, analyzer);
+                using IndexWriter indexWriter = new(indexDirectory, indexConfig);
+
+                if (indexWriter.NumDocs > 0)
+                {
+                    // Remove any existing index contents
+                    indexWriter.DeleteAll();
+                    indexWriter.Commit();
+                }
+
+                foreach (Guid id in episodeIds)
+                {
+                    EpisodeImport episodeImport = importContext.Episodes
+                        .Where(episode => episode.Id == id)
+                        .Single();
+
+                    Document document = IndexHelper
+                        .ConvertEpisode(episodeImport);
+
+                    indexWriter.AddDocument(document);
+                }
+
+                indexWriter.Flush(false, false);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.Error.WriteLine(ex.Message);
+                throw;
+            }
 
             // 4. Open export folder (Windows)
             Console.WriteLine($"DONE");
-            Console.WriteLine($"Finished indexing {documents.Count()} episodes in {stopwatchTotal.ElapsedMilliseconds} ms");
-            stopwatchTotal.Stop();
 
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 ProcessStartInfo processStartInfo = new()
                 {
                     FileName = @"c:\windows\explorer.exe",
-                    Arguments = exportPath,
+                    Arguments = settings.ExportFolderPath,
                     WindowStyle = ProcessWindowStyle.Normal,
                 };
                 Process.Start(processStartInfo);
             }
+        }
+
+        /// <summary>
+        /// Load the application configuration from user secrets or command-line arguments.
+        /// </summary>
+        /// <param name="args">The command-line arguments provided to the application.</param>
+        /// <returns>The bound <see cref="IndexBuilderSettings"/> model.</returns>
+        /// <exception cref="InvalidOperationException">The "IndexBuilderSettings" configuration section does not exist.</exception>
+        /// <exception cref="NullReferenceException">The "IndexBuilderSettings" configuration bound but a resulting setting was null.</exception>
+        private static IndexBuilderSettings LoadConfiguration(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .AddCommandLine(args)
+                .Build();
+
+            IndexBuilderSettings settings = new();
+            configuration
+                .GetRequiredSection(nameof(IndexBuilderSettings))
+                .Bind(settings);
+
+            if (string.IsNullOrWhiteSpace(settings.ImportDbPath))
+            {
+                throw new NullReferenceException("Couldn't load configuration for ImportDbPath");
+            }
+            else if (string.IsNullOrWhiteSpace(settings.ExportFolderPath))
+            {
+                throw new NullReferenceException("Couldn't load configuration for ExportDbPath");
+            }
+
+            return settings;
         }
     }
 }
