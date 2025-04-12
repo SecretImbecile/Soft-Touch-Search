7b9343568860a9826ca9a2c6c93db0325d7877b6
Jack Kelly <mail@jackkelly.uk>
2025-04-12 16:05:52 +0100

    Improved episode list (WIP)

---
+++ SoftTouchSearch/Extensions/DateTimeExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿namespace SoftTouchSearch.Extensions
+{
+    /// <summary>
+    /// Extension methods for <see cref="DateTime"/>.
+    /// </summary>
+    internal static class DateTimeExtensionMethods
+    {
+        /// <summary>
+        /// Returns the <see langword="string"/> representation of the <see cref="DateTime"/> in the format expected by an HTML datetime attribute.
+        /// </summary>
+        /// <param name="dateTime">The <see cref="DateTime"/> to format.</param>
+        /// <returns>The <see langword="string"/> representation of the <see cref="DateTime"/>.</returns>
+        public static string ToHtmlString(this DateTime dateTime)
+        {
+            return dateTime
+                .ToUniversalTime()
+                .ToString("yyyy-MM-dd'T'hh:mm:ssK");
+        }
+    }
+}
